e55064cd02a68d56bf8a1ebd36456fe840a6384f
Labtel <darwin.aponte@pucp.edu.pe>
2022-09-16 08:53:46 -0500

    holas

---
+++ app/src/main/java/edu/pucp/lab2_iot/ListarComputadoraActivity.java
@@ -0,0 +1,14 @@
+package edu.pucp.lab2_iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListarComputadoraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_computadora);
+    }
+}
