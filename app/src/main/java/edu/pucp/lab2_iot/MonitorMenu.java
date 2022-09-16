2d4cebfe39afbaf41c52187472888f89ef3023d9
pipipi <guillen.nicole@pucp.edu.pe>
2022-09-16 08:59:01 -0500

    Vistas Monitor

---
+++ app/src/main/java/edu/pucp/lab2_iot/MonitorMenu.java
@@ -0,0 +1,13 @@
+package edu.pucp.lab2_iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MonitorMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitor_menu);
+    }
+}
