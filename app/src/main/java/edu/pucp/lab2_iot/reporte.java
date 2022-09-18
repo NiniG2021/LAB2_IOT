09c22a6f71601c705652c54b12a6b81c3872b61e
NicoG <guillen.nicole@pucp.edu.pe>
2022-09-18 15:58:50 -0500

    reportept2

--- app/src/main/java/edu/pucp/lab2_iot/reporte.java
+++ app/src/main/java/edu/pucp/lab2_iot/reporte.java
@@ -2,6 +2,7 @@ package edu.pucp.lab2_iot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,18 +18,45 @@ import edu.pucp.lab2_iot.entity.ListaTeclados;
 
 public class reporte extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte);
 
-        //esto no
-        ListaMonitores listaMonitores = new ListaMonitores();
-        ListaTeclados listaTeclados = new ListaTeclados();
-        ListaComputadoras listaComputadoras = new ListaComputadoras();
 
-        //Ejemplo
-        ((TextView) findViewById(R.id.reporte_computadoras)).setText(ListaComputadoras.getListaComputadoras().size());
+        if(ListaComputadoras.getListaComputadoras().size() >0){
+            ((TextView)findViewById(R.id.reporte_computadoras)).setText(ListaComputadoras.getListaComputadoras().size());
+
+
+        }else{
+            TextView textView= findViewById(R.id.reporte_computadoras);
+            textView.setText("No se registra computadoras");
+
+        }
+
+        if(ListaMonitores.getlistamonitores().size() >0){
+            String cantidad_monitores = "Monitores: " + ListaMonitores.getlistamonitores().size();
+            ((TextView)findViewById(R.id.reporte_monitor)).setText(cantidad_monitores);
+
+        }else{
+            TextView textView= findViewById(R.id.reporte_monitor);
+            textView.setText("Monitores: No se registran monitores");
+
+        }
+
+        if( ListaTeclados.getListTeclados().size() > 0){
+            String cantidad_teclados = "Teclados: "+ListaTeclados.getListTeclados().size();
+            ((TextView)findViewById(R.id.reporte_teclado)).setText(cantidad_teclados);
+
+        }else{
+            TextView textView= findViewById(R.id.reporte_teclado);
+            textView.setText("Teclados: No se registran teclados");
+
+        }
+
+
+
 
 
 
