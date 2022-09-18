3fb58198d8e66bd7adf06acb99fd0544f2cd3dda
NicoG <guillen.nicole@pucp.edu.pe>
2022-09-18 14:19:12 -0500

    reportept1

---
+++ app/src/main/java/edu/pucp/lab2_iot/reporte.java
@@ -0,0 +1,37 @@
+package edu.pucp.lab2_iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import edu.pucp.lab2_iot.entity.ListaComputadoras;
+import edu.pucp.lab2_iot.entity.ListaMonitores;
+import edu.pucp.lab2_iot.entity.ListaTeclados;
+
+
+public class reporte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reporte);
+
+        ListaMonitores listaMonitores = new ListaMonitores();
+        ListaTeclados listaTeclados = new ListaTeclados();
+        ListaComputadoras listaComputadoras = new ListaComputadoras();
+
+
+
+
+
+    }
+
+
+
+}
